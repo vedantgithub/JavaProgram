ffc12cfc2d85831d39370fe050b25a391a641405
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-30 21:31:01 +0530

    6SwapTwoNo.java
    swapping two no. with temporary variable

---
+++ 6SwapTwoNo.java
@@ -0,0 +1,25 @@
+package JAVAProgram;
+
+public class SwappingTwoNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10,b=20;
+		int t;
+		System.out.println("before swapping a" +a);
+		System.out.println("before swapping b" +b);
+		System.out.println("\n");
+		t=a;
+		a=b;
+		b=t;
+		System.out.println("after swapping a" +a);
+		System.out.println("after swapping b" +b);
+		
+		
+		
+		
+
+	}
+
+}
