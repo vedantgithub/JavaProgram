6ef2985714d655aa69b2c144574fdb8a82c4baad
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-30 20:50:44 +0530

    LargeNoBwTwo.java
    B/w the two no. finding the largest no.

---
+++ LargeNoBwTwo.java
@@ -0,0 +1,19 @@
+package JAVAProgram;
+
+public class LargeNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20;
+		if(a>b)
+		{
+			System.out.println("A is greater");
+		}
+		else
+		{
+			System.out.println("B is greater");
+		}
+
+	}
+
+}
