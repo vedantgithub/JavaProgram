c6f24c66c6fbed703b2faca642144751351943f3
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-30 20:52:08 +0530

    LargeNoBwThree.java
    find the largest no. b/w the three no.

---
+++ LargeNoBwThree.java
@@ -0,0 +1,24 @@
+package JAVAProgram;
+
+public class LargeNoBWthree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c=10;
+		/*
+		if(a>b && a>c) {
+			System.out.println("A is greater");
+			}
+		else if(b>a && b>c)
+		{
+			System.out.println("B is greater");
+		}
+		else
+		{
+			System.out.println("C is greater");
+		}
+		
+		
+	}
+
+}
