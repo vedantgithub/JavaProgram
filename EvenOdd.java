8eb405fd11689313c7ab6a680467e011f5eef585
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-30 20:59:16 +0530

    EvenOdd.java
    check no. is even or odd

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+package JAVAProgram;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=90;
+		if(n%2==0)
+		{
+			System.out.println("No. is EVEN");
+		}
+		else
+		{
+			System.out.println("No. is ODD");
+		}
+
+	}
+
+}
