13d2743b6821abb69b261f87ed6800ce19bb04d6
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-31 12:43:43 +0530

    16Primeno1.java
    print prime ni from 1 to 100

---
+++ 16Primeno1.java
@@ -0,0 +1,30 @@
+package JAVAProgram;
+
+public class Prime1to100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		for(int no=1;no<=100;no++)
+		{
+			
+			int temp=0;;
+			for(int i=2;i<=no-1;i++)
+			{
+				if(no%i==0)
+				{
+					temp=temp+1;
+				}
+			}
+			if(temp==0)
+			{
+				System.out.println(no);
+			}
+			
+		}
+
+	
+	}
+
+}
