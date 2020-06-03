71462ee26b631ece8f5b37aa21fbdc6e1dc4cf6a
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-06-03 20:47:12 +0530

    Create 4JavaLoops1.java
    print its first 10 multiples

---
+++ Hacker Rank Java/4JavaLoops1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+
+            System.out.printf("%d x %d = %d%n", N, i, N*i);
+        }
+    }
+}
