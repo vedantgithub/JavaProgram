0b4e4788c78014b40669545e49a96b82130952e4
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-06-03 14:59:02 +0530

    Create DefaultConstructor.java
    compiler call the default const

---
+++ Constructor/DefaultConstructor.java
@@ -0,0 +1,16 @@
+package JAVAProgram;
+
+public class DefaultConst {
+	
+	int i;
+	String s;
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DefaultConst t=new DefaultConst();
+		System.out.println(t.i+" "+t.s);
+	}
+
+}
