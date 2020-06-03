29bf9309e0e95330a407c4c91db8f4c1f156d775
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-06-03 15:02:07 +0530

    Create UserdefineConstructor.java
    user define constructor

---
+++ Constructor/UserdefineConstructor.java
@@ -0,0 +1,16 @@
+package JAVAProgram;
+
+public class NoArgumentConst {
+	
+	NoArgumentConst()
+	{
+		System.out.println("No argument constructor");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		NoArgumentConst t=new NoArgumentConst();
+
+	}
+
+}
