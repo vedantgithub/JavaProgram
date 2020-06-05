6ed76ecdad817dadeb4d04c52182f1b344d2fee3
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-06-06 00:18:14 +0530

    Create 6pattern.java
     ****
      ***
       **
        *

---
+++ JavaPattern/6pattern.java
@@ -0,0 +1,19 @@
+package JAVAProgram;
+ public class pattern1 {
+
+		public static void main(String[] args) {
+			for(int i=1;i<=4;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(" ");
+				}
+				for(int k=4;k>=i;k--)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			
+		}
+ }
