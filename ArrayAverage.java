d1c3d4979c7444230bd8306c63d7e7b24f6d9bcc
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-08-05 21:47:58 +0530

    Create ArrayAverage.java

---
+++ ArrayAverage.java
@@ -0,0 +1,17 @@
+package vectorstack;
+
+public class array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int m[]= {1,2,3,4,5,6};
+		float avg = 0;
+		for(i=0;i<6;i++)
+		{
+			avg = avg + m[i];
+		}
+System.out.println(avg);
+	}
+
+}
