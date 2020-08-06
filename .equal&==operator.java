072950d3ca9e15b850e2acda695125e2f733218d
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-08-06 21:44:47 +0530

    Create .equal&==operator.java

---
+++ .equal&==operator.java
@@ -0,0 +1,22 @@
+package vectorstack;
+
+public class stringEqualop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1=new String("deepak");
+
+String s2=new String("deepak");
+
+System.out.println(s1==s2);    //false
+
+
+
+		String s3="deepak";
+		String s4="deepak";
+System.out.println(s3==s4);       //true
+
+System.out.println(s1.equals(s2)); //true
+	}
+
+}
